a02219681ea18dc2b0196afba552433d7eed511e
bogom <bogomolov222@gmail.com>
2021-02-06 20:05:12 +0300

    start app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header= () => {
+    return (
+        <div>
+            <img src="https://toppng.com/uploads/preview/hammer-png-11553953666sek7vnhpyq.png" />
+            <div>GitLab AutoTester</div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/logic/createTests.js
@@ -0,0 +1,48 @@
+
+class createTests {
+    _outputText
+
+    createStartTestFile(nameOfClass) {
+        this._outputText = "import org.junit.jupiter.api.BeforeEach;\n" +
+            "import org.junit.jupiter.api.Test;\n\n" +
+            "class " + nameOfClass + "Test" + " {\n"+
+            "private " + nameOfClass + " " + nameOfClass + "ClassTest\n";
+    }
+
+    addTestMethod(nameOfMethod, attributes) {
+        if(attributes != null){
+            return `
+            @Test\n
+            public void ${nameOfMethod}Test(${this.addAttributesFromMethod(attributes)}) {\n
+                AssertEquals() //todo
+            }\n`
+        }
+        else {
+            return `
+            @Test\n
+            public void ${nameOfMethod}Test() {\n
+                AssertEquals() //todo
+            }\n`
+        }
+    }
+
+    addAttributesFromMethod(attributes) {
+        let result = ``
+        for(let i = 0; i < attributes.length; i++) {
+            if(i+1 !== attributes.length){
+                result += `${attributes[i]}, `
+            }
+            else {
+                result += `${attributes[i]}`
+            }
+        }
+        return result
+    }
+
+
+
+    createFinishTestFile(){
+        this._outputText += "\n}\n";
+    }
+
+}
